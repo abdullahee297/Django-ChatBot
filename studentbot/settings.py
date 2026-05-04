a60ba4e7d2b9f65704bf46e7182c811bbbe4124e
Muhammad Abdullah <232841637+abdullahee297@users.noreply.github.com>
2026-05-04 17:37:38 +0500

    Update settings.py

--- studentbot/settings.py
+++ studentbot/settings.py
@@ -54,9 +54,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
