773df301edbc237bc3a6cace96aa7a1409fd8acc
Muhammad Abdullah <232841637+abdullahee297@users.noreply.github.com>
2026-05-04 16:43:43 +0500

    Deployment

--- api/index.py
+++ api/index.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qrcodegenerator.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studentbot.settings")
 
-=======
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qrcodegenerator.settings")
-
->>>>>>> ec58928 (Crash Error Solve)
 app = get_wsgi_application()
